62140efff3f631ba51b636b3c91a81898238a6b9
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2023-03-27 12:35:27 +0200

    contact emailjs & Stars canvas

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,9 +16,52 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => { };
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
 
-  const handleSubmit = (e) => { };
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+      // import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      // import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      // {
+      //   from_name: form.name,
+      //   to_name: "Jean Paul",
+      //   from_email: form.email,
+      //   to_email: "my@email.com",
+      //   message: form.message,
+      // },
+      // import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  };
 
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
